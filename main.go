b990217ef969dd001353c0fd49e6e4bb7da0d490
核善 <heshan@qunhemail.com>
2024-09-04 16:03:20 +0800

    feat: 使用新的方式加速

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
 	"sync"
 	"time"
 
@@ -15,7 +13,7 @@ import (
 var middlePressed = false
 var lastClearTimer *time.Timer
 
-var lastMoveTimer *time.Timer
+// var lastMoveTimer *time.Timer
 var startX int16
 
 var startY int16
@@ -25,88 +23,90 @@ var lastClearTimerChan = make(chan int)
 var rwMutex sync.RWMutex
 
 func main() {
+
+	var lastXChan = make(chan int16)
+	var lastYChan = make(chan int16)
+
 	hook.Register(hook.MouseDown, []string{}, func(e hook.Event) {
-		fmt.Println("numbers: ", runtime.NumGoroutine())
+		// fmt.Println("numbers: ", runtime.NumGoroutine())
 		if e.Button == 3 {
 			if lastClearTimer != nil {
 				if lastClearTimer.Stop() {
 					lastClearTimerChan <- 1
 				}
 			}
+			startX = e.X
+			startY = e.Y
 			middlePressed = true
 			go func() {
 				// 定时器关闭
-				lastClearTimer = time.NewTimer(time.Second * 1)
-				select {
-				case <-lastClearTimer.C:
+				rwMutex.Lock()
+				lastClearTimer = time.NewTimer(time.Millisecond * 400)
+				rwMutex.Unlock()
+				lastX := int16(0)
+				lastY := int16(0)
+				loop := true
+				var exit = func() {
 					middlePressed = false
-				case <-lastClearTimerChan:
+
+					go func() {
+						time.Sleep(time.Millisecond * 10)
+						loop = false
+						lastClearTimer = nil
+					}()
+
+				}
+				var check = func() {
+					if !middlePressed {
+						return
+					}
+					finalX := lastX
+					finalY := lastY
+					arr := []string{"lctrl", "cmd"}
+					needExit := false
+					if finalX-startX >= 150 {
+						needExit = true
+						robotgo.KeyTap("right", arr)
+					} else if startX-finalX >= 150 {
+						needExit = true
+						robotgo.KeyTap("left", arr)
+					} else if startY-finalY >= 150 {
+						needExit = true
+						robotgo.KeyTap("tab", []string{"cmd"})
+					} else if finalY-startY >= 150 {
+						needExit = true
+						robotgo.KeyTap("tab", []string{"lalt", "ctrl"})
+					}
+					if needExit {
+						exit()
+					}
+				}
+
+				for loop {
+					select {
+					case <-lastClearTimer.C:
+						exit()
+					case <-lastClearTimerChan:
+						exit()
+					case lastXIn := <-lastXChan:
+						lastX = lastXIn
+					case lastYIn := <-lastYChan:
+						lastY = lastYIn
+						check()
+					}
 				}
 			}()
 		}
 	})
 
-	var lastXChan = make(chan int16)
-	var lastYChan = make(chan int16)
-
 	hook.Register(hook.MouseMove, []string{}, func(e hook.Event) {
 		if middlePressed {
-			if lastMoveTimer == nil {
-				startX = e.X
-				startY = e.Y
-				lastMoveTimer = time.NewTimer(time.Millisecond * 400)
-				go func() {
-					lastX := int16(0)
-					lastY := int16(0)
-					var loop = true
-					for loop {
-						select {
-						case <-lastMoveTimer.C:
-							rwMutex.Lock()
-							defer rwMutex.Unlock()
-
-							finalX := lastX
-							finalY := lastY
-							if finalX == 0 || finalY == 0 {
-								loop = false
-								lastMoveTimer = nil
-								middlePressed = false
-								return
-							}
-							arr := []string{"lctrl", "cmd"}
-
-							if finalX-startX >= 150 {
-								robotgo.KeyTap("right", arr)
-								fmt.Println("right")
-							} else if startX-finalX >= 150 {
-								robotgo.KeyTap("left", arr)
-								fmt.Println("left")
-							} else if startY-finalY >= 150 {
-								robotgo.KeyTap("tab", []string{"cmd"})
-							} else if finalY-startY >= 150 {
-								robotgo.KeyTap("tab", []string{"lalt", "ctrl"})
-							}
-							lastMoveTimer = nil
-							middlePressed = false
-
-							loop = false
-							return
-						case lastXIn := <-lastXChan:
-							lastX = lastXIn
-						case lastYIn := <-lastYChan:
-							lastY = lastYIn
-
-						}
-					}
-				}()
-			} else {
-				rwMutex.RLock()
-				if lastMoveTimer != nil {
-					lastXChan <- e.X
-					lastYChan <- e.Y
-				}
-				rwMutex.RUnlock()
+			rwMutex.RLock()
+			if lastClearTimer != nil {
+				lastXChan <- e.X
+				lastYChan <- e.Y
 			}
+			rwMutex.RUnlock()
 		}
 	})
 
